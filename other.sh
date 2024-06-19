17b6762ac0bec2ac52d51bd017659d18a73ec837
SystemJargon <24641464+SystemJargon@users.noreply.github.com>
2024-06-20 07:30:27 +1200

    Update other.sh

--- other.sh
+++ other.sh
@@ -1,1 +1,3 @@
-# other actions in this file, where not defined in any other script
+### other actions in this file, where not defined in any other script
+# import nftables rules file
+sudo nft -f nftables.rules
